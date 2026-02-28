73b6a37b6cf2297ea18c10c7a8cfe863f43f5809
Fajar Sebastian S <fajarss1705@gmail.com>
2026-02-28 16:09:42 +0700

    Add phone_number, avatar, jabatan to User TypeScript type

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -2,7 +2,9 @@ export type User = {
     id: number;
     name: string;
     email: string;
-    avatar?: string;
+    phone_number?: string | null;
+    avatar?: string | null;
+    jabatan?: string | null;
     email_verified_at: string | null;
     two_factor_enabled?: boolean;
     created_at: string;
