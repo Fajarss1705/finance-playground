5a15937e952d16a03fee5ac39c106579f9aa4c04
Fajar Sebastian S <fajarss1705@gmail.com>
2026-03-03 16:38:14 +0700

    Add admin org/team feature test seeder and update DatabaseSeeder
    Chain FeatureTestAdminUserManagement20260303Seeder from
    FeatureTestAdminRolePermission20260303Seeder. Adds admin.users.index
    permission to Sekretaris role for read-only access.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            FeatureTestAdminRolePermission20260303Seeder::class,
+            FeatureTestAdminUserManagement20260303Seeder::class,
         ]);
     }
 }
